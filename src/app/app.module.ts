4f5a0adbd207c86319e798f1a4f40a5a94670053
KZYB <krzysztof.zybul@gmail.com>
2019-07-23 21:23:33 +0200

    [ANG-GOL]Added board generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { GolDashboardComponent } from './gol-dashboard/gol-dashboard.component';
 import { DnaComponent } from './dna/dna.component';
--- src/app/gol-dashboard/gol-dashboard.component.ts
+++ src/app/gol-dashboard/gol-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import {NgForm} from '@angular/forms';
 import { Cell } from './cell';
 
 @Component({
@@ -6,9 +7,10 @@ import { Cell } from './cell';
   templateUrl: './gol-dashboard.component.html',
   styleUrls: ['./gol-dashboard.component.css']
 })
-export class GolDashboardComponent implements OnInit {
-  xSize: number = 0;
-  ySize: number = 0;
+export class GolDashboardComponent implements OnInit, OnChanges {
+  xSize: number;
+  ySize: number;
+  cells: Cell[][]=[]; 
 
   cell: Cell = {
     coordX: 0,
@@ -19,6 +21,29 @@ export class GolDashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log("SIZE:", this.cells.length);
   }
 
+  ngOnChanges(){
+  }
+
+  onSubmit(dashboardXY: NgForm) {
+    this.cells=[];
+    console.log("LOG: ", dashboardXY.value);
+    
+    this.xSize = dashboardXY.value.xSize;
+    this.ySize = dashboardXY.value.ySize;
+
+    for (let indexY = 0; indexY < this.ySize; indexY++) {
+      let element: Cell[] = [];
+      this.cells.push(element);
+    }
+
+    for (let indexY = 0; indexY < this.ySize; indexY++) {
+      for (let indexX = 0; indexX < this.xSize; indexX++) {
+        this.cells[indexY][indexX] = {coordX: indexX, coordY: indexY, isAlive: false};
+        console.log("Creating: ", this.cells[indexY][indexX]);
+      }
+    }
+  }
 }
